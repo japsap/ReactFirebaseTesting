622489a3678b78cf9ac2c2143d576c6220ef2638
Aleksander Ivanov <alexmmech@gmail.com>
2022-08-08 17:40:49 +0300

    some something

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,33 +1,74 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+import { DummyData } from "../Data/DummyData";
+
+//bootstrap
+import Image from "react-bootstrap/Image";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+//import img
+import logo from "../image/logo.png";
+
+const Navbar = (props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className='navbar__component'>
-        
-        {/* navbar mobile */}
-        <div className='navbar__mobile'>
+    <div className="navbar__component">
+      {/* navbar mobile */}
+      <div className="navbar__mobile"></div>
+      {/* navbar mobile */}
 
+      {/* naavbar pc */}
+      <div className="navbar__pc">
+        <div className="navbar__logo">
+          <Image src={logo} fluid />
         </div>
-        {/* navbar mobile */}
-
-        {/* naavbar pc */}
-        <div className='navbar__pc'>
-            <div className='navbar__logo'>
-
-            </div>
-            <div className='navbar__links'>
-                {/* <li><Link></Link></li> */}
-            </div>
-
-            <div className='navbar__user'>
-                
-            </div>
+        <div className="navbar__links">
+          {DummyData.navbarLinks.map((link) => (
+            <li key={link.id}>
+              <Link to={link.path}>{link.name}</Link>
+            </li>
+          ))}
         </div>
-        {/* naavbar pc */}
 
+        <div className="navbar__user">
+          <button className="button__navbar" onClick={handleShow}>
+            {DummyData.navLogButton.title}
+          </button>
+        </div>
+      </div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            <h1 className="text-center">Log in</h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+            ac consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+      {/* naavbar pc */}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/Data/DummyData.js
+++ client/src/Data/DummyData.js
@@ -1,36 +1,38 @@
-export const DummyData = [
-    {
-        id: 1,
-        name: "Home",
-        path: "/"
-    },
-    {
-        id: 2,
-        name: "Genre",
-        path: "/"
-    },
-    {
-        id: 3,
-        name: "Country",
-        path: "/"
-    },
-    {
-        id: 4,
-        name: "Movies",
-        path: "/"
-    },
-    {
-        id: 5,
-        name: "TV Shoes",
-        path: "/"
-    },
-    {
-        id: 6,
-        name: "Top IMDB",
-        path: "/"
-    },
-
+export const DummyData = {
+    navbarLinks : [
+        {
+            id: 1,
+            name: "Home",
+            path: "/"
+        },
+        {
+            id: 2,
+            name: "Genre",
+            path: "/"
+        },
+        {
+            id: 3,
+            name: "Country",
+            path: "/"
+        },
+        {
+            id: 4,
+            name: "Movies",
+            path: "/"
+        },
+        {
+            id: 5,
+            name: "TV Shoes",
+            path: "/"
+        },
+        {
+            id: 6,
+            name: "Top IMDB",
+            path: "/"
+        }
+    ],
     navLogButton : {
-        title: ""
+        title: "Log in"
     }
-]
+}
+  
